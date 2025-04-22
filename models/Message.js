fc308abe18ee021f4b369ef90bfaae4413ae82ae
Prathamesh Patil <91467888+1-apex@users.noreply.github.com>
2025-04-22 11:21:56 -0500

    Add files via upload

---
+++ models/Message.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    chatroomId: {
+        type: String, // Use Firestore chatroom ID
+        required: true
+    },
+    senderId: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
